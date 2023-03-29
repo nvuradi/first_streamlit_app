57a12dafdf3b5eee28517fe5cfb684db40d5c1ca
nvuradi <nvuradi@gmail.com>
2023-03-29 16:18:42 -0500

    Update streamlit_app.py
    added header, subheader and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
-import streamlit
+import streamlit as st
 
-streamlit.title('My Parents New Healthy Diner')
+st.title('My Parents New Healthy Diner')
+st.header('Breakfast Menu')
+st.subheader('Eggs')
+st.text('Hard-Boiled Free-Range Egg')
+st.subheader('Drinks')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.subheader('Oatmeal')
+st.text('Omega 3 & Blueberry Oatmeal')
