52258d15aab0cbd96ac2d77039a708ff6d130725
nvuradi <nvuradi@gmail.com>
2023-03-29 16:27:07 -0500

    Update streamlit_app.py
    added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-st.title('My Parents New Healthy Diner')
+st.title("My Mom's New Healthy Diner")
 st.header('Breakfast Favorites')
 #st.subheader('Oatmeal')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -14,4 +14,5 @@ st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 st.dataframe(my_fruit_list)
