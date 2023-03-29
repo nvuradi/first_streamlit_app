7744f6c98fd92d112b27913fcbb55cb934732bad
nvuradi <nvuradi@gmail.com>
2023-03-29 16:22:26 -0500

    Update streamlit_app.py
    re-arranged the menu, added emojis and additional breakfast item

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 
 st.title('My Parents New Healthy Diner')
-st.header('Breakfast Menu')
-st.subheader('Eggs')
-st.text('Hard-Boiled Free-Range Egg')
-st.subheader('Drinks')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.subheader('Oatmeal')
-st.text('Omega 3 & Blueberry Oatmeal')
+st.header('Breakfast Favorites')
+#st.subheader('Oatmeal')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+#st.subheader('Drinks')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+#st.subheader('Eggs')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
