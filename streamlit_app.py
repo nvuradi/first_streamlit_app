f1ff9eb21e3f8f2e2b111c1dc4050ece9faef151
nvuradi <nvuradi@gmail.com>
2023-03-30 17:02:00 -0500

    Update streamlit_app.py
    added requests to get the fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import requests as r
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -16,4 +17,6 @@ st.text('🥑🍞 Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_selected=st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruityvice_response = r.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 st.dataframe(fruits_to_show)
