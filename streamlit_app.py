27dc722b82ba44f6be39a15653c2c7ddb776e376
nvuradi <nvuradi@gmail.com>
2023-03-31 16:45:40 -0500

    Update streamlit_app.py
    added stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,5 @@ add_my_fruit=st.text_input('What fruit would you like do?','Jackfruit')
 st.write('The user entered ', add_my_fruit)
 
 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values ('from st')");
+
+st.stop();
