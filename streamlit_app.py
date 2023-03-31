1ed4840e53832ce4a2bfaba11866bb1bdd7bd927
nvuradi <nvuradi@gmail.com>
2023-03-31 17:21:45 -0500

    Update streamlit_app.py
    ad

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ except URLError as e:
   st.error()
 
 #add snowflake information
-st.header("The fruit load list contains:")
+st.header("View Our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
